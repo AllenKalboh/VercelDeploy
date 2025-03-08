45219c58382f0c72f47e1689fe67ca533499e57e
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2025-03-08 11:07:39 +0800

    try Send

--- src/Register.tsx
+++ src/Register.tsx
@@ -8,23 +8,29 @@ import { supabase } from './components/CreateClient'
 
 const Register = () => {
 
-    const [name, setName] = useState<string | null>(null)
+    // const [name, setName] = useState<string | null>(null)
     const [message, setMessage] = useState<string | null>(null)
     // const [user, setUser] = useState<User | null>(null)
-
-    const handleNameChange = (e: any) => {
-        setName(e.target.value)
-    }
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    // const handleNameChange = (e: any) => {
+    //     setName(e.target.value)
+    // }
 
     const handleMessageChange = (e: any) => {
         setMessage(e.target.value)
     }
 
-    const insertUser = async () => {
-        await supabase.from('YowSer').insert({
-            name: name,
+    const insertMessage = async () => {
+        setIsLoading(true)
+        const { error } = await supabase.from('YowSer').insert({
             message: message
-        })
+        });
+        if (error) {
+            alert('Dili nag send ang imong messageji')
+        } else {
+           setIsLoading(false)
+        }
+
     }
 
     // const fetchMessage = async () =>{
@@ -34,32 +40,36 @@ const Register = () => {
 
     return (
         <div>
+           
+            {isLoading && <p className='m-2 p-2'>Loading...</p>}
+            <h1 className='m-2 p-2 font-bold'>Say something to Allen</h1>
             <input
                 type="text"
-                placeholder='name'
-                value={name ?? ""}
-                className='p-2 m-2 border-2 rounded-2xl'
-                onChange={handleNameChange} />
-
-            <input
-                type="text"
-                placeholder='message'
+                placeholder='loeee'
                 value={message ?? ""}
                 className='p-2 m-2 border-2 rounded-2xl'
-                onChange={handleMessageChange} />
+                onChange={handleMessageChange}
+                required />
 
             <button className='border-2 p-2 m-2 rounded-2xl w-30 hover:bg-amber-100 hover:cursor-pointer duration-300'
-                onClick={insertUser}>
-                Submit
+                onClick={insertMessage}>
+
+
+
+             {isLoading ? "Sending..." : "Submit"}
             </button>
 
-            <p>
+            {/* <p>
                 Name:    {name}
-            </p>
-
-            <p>
-                Message: {message}
-            </p>
+            </p> */}
+            <div className='m-4 rounded-2xl'>
+                {/* <p className='m-2 p-2 break-words'>
+                    Dear Allen, */}
+                    <p className=''>
+                       Message: {message}
+                    </p>
+                {/* </p> */}
+            </div>
 
 
         </div>
