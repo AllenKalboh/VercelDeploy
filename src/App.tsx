7c5c30aba9a798dfdf16fd02c77ab35d62027d47
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2025-03-08 10:13:57 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold p-2 m-2 text-center ">
-   Hello anjiel
-  </h1>
+      <h1 className="text-3xl font-bold p-2 m-2 text-center bg-red-500 ">
+        Eto yung Vercel App ko
+      </h1>
     </div>
   )
 }
