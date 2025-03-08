b99c05c64651a8b70416e8abf9892592af891945
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2025-03-08 10:09:21 +0800

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold p-2 m-2 text-center ">
-    Hello world! This is my First React App Hosted in Vercel hehe
+   Hello anjiel
   </h1>
     </div>
   )
