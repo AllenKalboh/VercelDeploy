ecd121c7191e56bedd2c17f91ab22daf21ee5c72
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2025-03-17 19:53:05 +0800

    updaet

--- src/Register.tsx
+++ src/Register.tsx
@@ -4,6 +4,7 @@ import { supabase } from './components/CreateClient'
 const Register = () => {
 
     const [message, setMessage] = useState<string | null>(null)
+    const [name, setName] = useState<string>("")
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
 
@@ -11,13 +12,18 @@ const Register = () => {
         setMessage(e.target.value)
     }
 
+    const handleNameChange = (e: any) => {
+        setName(e.target.value)
+    }
+
     const insertMessage = async () => {
         setIsLoading(true)
         const { error } = await supabase.from('YowSer').insert({
+            name: name,
             message: message
         });
-        if (!message || message.trim() === "") {
-            alert("maglagay ka raw nga ng message ngaaa");
+        if (!message || message.trim() === "" && name || name.trim() === "") {
+            alert("May kulang daw nga na field nga");
             setIsLoading(false)
             return; // Stop execution if message is empty
         }
@@ -26,7 +32,7 @@ const Register = () => {
             setIsLoading(false)
         } else {
             setIsLoading(false)
-            alert('nag send na boi')
+            alert('nag send na okay na haha')
             setMessage("")
         }
 
@@ -38,14 +44,26 @@ const Register = () => {
                 <h1 className='mt-6 p-4 font-bold text-white text-3xl'>Say something to Allen</h1>
             </div>
             <div className='flex flex-col justify-center items-center'>
+
+            <input
+                    required
+                    type="text"
+                    placeholder='Enter your Name'
+                    value={name ?? ""}
+                    className='p-2 m-1 text-2xl text-white border-2 border-white rounded-2xl'
+                    onChange={handleNameChange}
+                />
+
                 <input
                     required
                     type="text"
                     placeholder='type something...'
                     value={message ?? ""}
-                    className='p-2 m-4 text-white border-2 border-white rounded-2xl'
+                    className='p-2 m-4 text-2xl text-white border-2 border-white rounded-2xl'
                     onChange={handleMessageChange}
                 />
+
+                
 
                 <button className='border-2 border-white text-white p-2 m-2 rounded-2xl w-30 hover:bg-white hover:cursor-pointer hover:text-[#384B70] duration-300'
                     onClick={insertMessage}>
@@ -59,7 +77,7 @@ const Register = () => {
                 {/* <p className='m-2 p-2 break-words'>
                     Dear Allen, */}
                 <div className='text-white break-words text-4xl'>
-                    Messagej: <p className='italic indent-8'> {message} </p>
+                    Message: <p className='italic indent-8'> {message} </p>
                 </div>
                 {/* </p> */}
             </div>
