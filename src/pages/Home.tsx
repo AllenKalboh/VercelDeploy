23c63d0f270c8968b8ad3cb6f86f912d8320c1c9
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2025-03-22 20:07:31 +0800

    Fex

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import {Link} from 'react-router-dom';
 const Home = () => {
   return (
-    <div>
-      <Link to="/register"> Register </Link>
-      <Link  to="/login"> Login </Link>
+    <div className='flex flex-col p-2 m-2'>
+      <Link to="/register" className='bg-gray-500 w-40 p-2 m-2 text-white hover:bg-gray-700 duration-300'> Register </Link>
+      <Link  to="/login" className='bg-gray-500 w-40 p-2 m-2 text-white hover:bg-gray-700 duration-300'> Login </Link>
     </div>
   )
 }
